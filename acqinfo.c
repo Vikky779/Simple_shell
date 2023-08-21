c49b65294da85dbbbcffc0fce67147127ec8c4c7
Ecclesias01 <ecclesias01@gmail.com>
2023-08-21 08:27:27 -0700

    to get info

---
+++ acqinfo.c
@@ -0,0 +1,74 @@
+#include "shell.h"
+
+/**
+* clr_inf - initializes inf_t struct
+* @inf: struct address
+*/
+void clr_inf(info_t *inf)
+{
+	inf->arg = NULL;
+	inf->argv = NULL;
+	inf->path = NULL;
+	inf->argc = 0;
+}
+
+/**
+* set_inf - initializes inf_t struct
+* @inf: struct address
+* @bf: argument vector
+*/
+void set_inf(info_t *inf, char **bf)
+{
+	int e = 0;
+
+	inf->fname = bf[0];
+	if (inf->arg)
+	{
+	inf->argv = strtow(inf->arg, " \t");
+	if (!inf->argv)
+	{
+	inf->argv = malloc(sizeof(char *) * 2);
+	if (inf->argv)
+	{
+	inf->argv[0] = _strdup(inf->arg);
+	inf->argv[1] = NULL;
+	}
+	}
+	for (e = 0; inf->argv && inf->argv[e]; e++)
+		;
+	inf->argc = e;
+
+	replace_alias(inf);
+	replace_vars(inf);
+	}
+}
+
+/**
+* free_inf - frees inf_t struct fields
+* @inf: struct address
+* @tod: true if freeing all fields
+*/
+void free_inf(info_t *inf, int tod)
+{
+	ffree(inf->argv);
+	inf->argv = NULL;
+	inf->path = NULL;
+	if (tod)
+	{
+	if (!inf->cmd_buf)
+	free(inf->arg);
+	if (inf->env)
+	free_list(&(inf->env));
+	if (inf->history)
+	free_list(&(info->history));
+	if (inf->alias)
+	free_list(&(inf->alias));
+	ffree(inf->environ);
+	inf->environ = NULL;
+	bfree((void **)inf->cmd_buf);
+	if (inf->readfd > 2)
+	close(inf->readfd);
+	_putchar(BUF_FLUSH);
+	}
+}
+
--- shell_string1.c
+++ shell_string1.c
@@ -1,50 +1,51 @@
 #include "shell.h"
+
 /**
- * _strcpy - This function copies a string
- * @t: destination string
- * @s: source string
- * Return: A pointer to destination
- */
+* _strcpy - This function copies a string
+* @t: destination string
+* @s: source string
+* Return: A pointer to destination
+*/
 char *_strcpy(char *t, char *s)
 {
 	int k = 0;
 
 	if (t == s || s == 0)
-		return (t);
+	return (t);
 	while (s[k])
 	{
-		t[k] = s[k];
-		k++;
+	t[k] = s[k];
+	k++;
 	}
 	t[k] = 0;
 	return (t);
 }
 /**
- * _strdup - This duplicates a string
- * @s: String to be duplicated
- * Return: A pointer to the duplicated string
- */
+* _strdup - This duplicates a string
+* @s: String to be duplicated
+* Return: A pointer to the duplicated string
+*/
 char *_strdup(const char *s)
 {
 	int h = 0;
 	char *dupl;
 
 	if (s == NULL)
-		return (NULL);
+	return (NULL);
 	while (*s++)
-		h++;
+	h++;
 	dupl = malloc(sizeof(char) * (h + 1));
 	if (!dupl)
-		return (NULL);
+	return (NULL);
 	for (h++; h--;)
-		dupl[h] = *--s;
+	dupl[h] = *--s;
 	return (dupl);
 }
 /**
- *_puts - This prints an input string
- *@s: String to be printed
- * Return: Nothing
- */
+*_puts - This prints an input string
+*@s: String to be printed
+* Return: Nothing
+*/
 void _puts(char *s)
 {
 	int k = 0;
@@ -53,16 +54,16 @@ void _puts(char *s)
 		return;
 	while (s[k] != '\0')
 	{
-		_putchar(s[k]);
-		k++;
+	_putchar(s[k]);
+	k++;
 	}
 }
 
 /**
- * _putchar - This writes the character c to stdout
- * @c: Character to be  printed
- * Return: On success 1 and  error, -1 is returned
- */
+* _putchar - This writes the character c to stdout
+* @c: Character to be  printed
+* Return: On success 1 and  error, -1 is returned
+*/
 int _putchar(char c)
 {
 	static int k;
@@ -70,10 +71,10 @@ int _putchar(char c)
 
 	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
 	{
-		write(1, buf, k);
-		k = 0;
+	write(1, buf, k);
+	k = 0;
 	}
 	if (c != BUF_FLUSH)
-		buf[k++] = c;
+	buf[k++] = c;
 	return (1);
 }
